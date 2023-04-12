2c26f17f5e95173ed4112081efec4f330333ecee
Ben Brandt <benjamin.j.brandt@gmail.com>
2023-04-12 11:25:05 +0200

    Cleanup test book and add another short chunk testcase

--- tests/snapshot_tests.rs
+++ tests/snapshot_tests.rs
@@ -3,9 +3,17 @@ use std::fs;
 use text_splitter::TextSplitter;
 
 #[test]
-fn paragraph_snapshot() {
+fn paragraph_long_chunk() {
     let text = fs::read_to_string("tests/room_with_a_view.txt").unwrap();
     let splitter = TextSplitter::new(1000);
+    let chunks = splitter.chunk_by_paragraphs(&text).collect::<Vec<_>>();
+    insta::assert_yaml_snapshot!(chunks);
+}
+
+#[test]
+fn paragraph_short_chunk() {
+    let text = fs::read_to_string("tests/room_with_a_view.txt").unwrap();
+    let splitter = TextSplitter::new(100);
     let chunks = splitter.chunk_by_paragraphs(&text).collect::<Vec<_>>();
     insta::assert_yaml_snapshot!(chunks);
 }
