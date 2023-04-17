4c285467f12fe1bbe1a1a12042f6c9279436df50
Ben Brandt <benjamin.j.brandt@gmail.com>
2023-04-17 09:51:29 +0200

    Split up tests

---
+++ tests/char_length_snapshots.rs
@@ -0,0 +1,69 @@
+use std::fs;
+
+use text_splitter::TextSplitter;
+
+#[test]
+fn paragraph_long_chunk() {
+    let text = fs::read_to_string("tests/room_with_a_view.txt").unwrap();
+    let splitter = TextSplitter::new(1000);
+    let chunks = splitter
+        .chunk_by_paragraphs(&text)
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+    insta::assert_yaml_snapshot!(chunks);
+}
+
+#[test]
+fn paragraph_short_chunk() {
+    let text = fs::read_to_string("tests/room_with_a_view.txt").unwrap();
+    let splitter = TextSplitter::new(100);
+    let chunks = splitter
+        .chunk_by_paragraphs(&text)
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+    insta::assert_yaml_snapshot!(chunks);
+}
+
+#[test]
+fn paragraph_tiny_chunk() {
+    let text = fs::read_to_string("tests/room_with_a_view.txt").unwrap();
+    let splitter = TextSplitter::new(10);
+    let chunks = splitter
+        .chunk_by_paragraphs(&text)
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+    insta::assert_yaml_snapshot!(chunks);
+}
+
+#[test]
+fn paragraph_long_chunk_trim() {
+    let text = fs::read_to_string("tests/room_with_a_view.txt").unwrap();
+    let splitter = TextSplitter::new(1000).with_trim_chunks(true);
+    let chunks = splitter
+        .chunk_by_paragraphs(&text)
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+    insta::assert_yaml_snapshot!(chunks);
+}
+
+#[test]
+fn paragraph_short_chunk_trim() {
+    let text = fs::read_to_string("tests/room_with_a_view.txt").unwrap();
+    let splitter = TextSplitter::new(100).with_trim_chunks(true);
+    let chunks = splitter
+        .chunk_by_paragraphs(&text)
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+    insta::assert_yaml_snapshot!(chunks);
+}
+
+#[test]
+fn paragraph_tiny_chunk_trim() {
+    let text = fs::read_to_string("tests/room_with_a_view.txt").unwrap();
+    let splitter = TextSplitter::new(10).with_trim_chunks(true);
+    let chunks = splitter
+        .chunk_by_paragraphs(&text)
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+    insta::assert_yaml_snapshot!(chunks);
+}
