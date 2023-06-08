cdfe61ef1bf5f4e330602efe0f97b86cea0f263a
Ben Brandt <benjamin.j.brandt@gmail.com>
2023-06-08 21:43:17 +0200

    Lint python rust code

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -1,3 +1,19 @@
+//! Python Bindings for text-splitter crate
+
+#![warn(
+    clippy::pedantic,
+    future_incompatible,
+    missing_debug_implementations,
+    missing_docs,
+    nonstandard_style,
+    rust_2018_compatibility,
+    rust_2018_idioms,
+    rust_2021_compatibility,
+    unused
+)]
+// pyo3 uses this
+#![allow(elided_lifetimes_in_paths)]
+
 use ::text_splitter::{Characters, ChunkCapacity, TextSplitter};
 use pyo3::prelude::*;
 
