fb219209252de59143715fe3719f03ddc729c0ee
Ben Brandt <benjamin.j.brandt@gmail.com>
2024-01-20 16:29:07 +0100

    Fix small chunk behavior (#84)
    * Make less tests fail without features
    
    * Fix for regression not being fixed for tiny chunk sizes
    
    For very small chunk sizes (i.e. 5 tokens), the chunk size behavior wasn't completely brought back to pre-v0.5.0 behavior. While sizes of 10 or higher seemed to be unaffected, smaller had a higher chance of seeing this occaisional bug. While an edge case, the behavior is fixed now.
    
    * Exclude tokenizers from packaging
    
    * Readable tokenizer file

--- tests/text_splitter.rs
+++ tests/text_splitter.rs
@@ -128,3 +128,16 @@ fn random_chunk_range() {
         }
     }
 }
+
+#[cfg(feature = "tokenizers")]
+#[test]
+fn huggingface_small_chunk_behavior() {
+    let tokenizer =
+        tokenizers::Tokenizer::from_file("./tests/tokenizers/huggingface.json").unwrap();
+    let splitter = TextSplitter::new(tokenizer);
+
+    let text = "notokenexistsforthisword";
+    let chunks = splitter.chunks(text, 5).collect::<Vec<_>>();
+
+    assert_eq!(chunks, ["notokenexistsforth", "isword"]);
+}
