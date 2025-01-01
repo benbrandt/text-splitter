1aa902a9bae44a806cde1b2e4fd80e4176c39682
Ben Brandt <benjamin.j.brandt@gmail.com>
2025-01-01 20:45:32 +0100

    Attempt to fix readthedocs config

---
+++ conf.py
@@ -0,0 +1,11 @@
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'semantic-text-splitter'
+copyright = '2023-2025, Ben Brandt'
+author = 'Ben Brandt'
+
+# -- General configuration ------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+language = 'en'
