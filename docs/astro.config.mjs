93604d721575f74e79e9984e11df8c5f5192394e
Ben Brandt <benjamin.j.brandt@gmail.com>
2025-05-14 09:49:09 +0200

    update social config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,34 +1,41 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://benbrandt.github.io',
-  base: 'text-splitter',
-	integrations: [
-		starlight({
-			title: 'text-splitter',
-			social: {
-				github: 'https://github.com/benbrandt/text-splitter',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-					// items: [
-					// 	// Each item here is one entry in the navigation menu.
-					// 	{ label: 'Example Guide', link: '/guides/example/' },
-					// ],
-				},
-				{
-					label: 'Reference',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Rust API', link: 'https://docs.rs/text-splitter' },
-						{ label: 'Python API', link: 'https://semantic-text-splitter.readthedocs.io/en/latest/semantic_text_splitter.html' },
-					],
-				},
-			],
-		}),
-	],
+  site: "https://benbrandt.github.io",
+  base: "text-splitter",
+  integrations: [
+    starlight({
+      title: "text-splitter",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/benbrandt/text-splitter",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+          // items: [
+          // 	// Each item here is one entry in the navigation menu.
+          // 	{ label: 'Example Guide', link: '/guides/example/' },
+          // ],
+        },
+        {
+          label: "Reference",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Rust API", link: "https://docs.rs/text-splitter" },
+            {
+              label: "Python API",
+              link: "https://semantic-text-splitter.readthedocs.io/en/latest/semantic_text_splitter.html",
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 });
